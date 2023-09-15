3f4a8fd0e0d36f2c9d5a5bef67afe03af41e2aa6
dashoes15 <daltonasali@gmail.com>
2023-09-15 09:19:09 -0400

    lab4 part3

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scan = new Scanner(System.in);
+        int springCost;
+        int summerCost;
+        int fallCost;
+        int winterCost;
+        int yearlyCost;
+        System.out.println("Input spring maintencence cost");
+        springCost = scan.nextInt();
+        System.out.println("Input summer maintenence cost");
+        summerCost = scan.nextInt();
+        System.out.println("Input fall maintence cost");
+        fallCost = scan.nextInt();
+        System.out.println("Input winter maintenence cost");
+        winterCost = scan.nextInt();
+        yearlyCost = springCost+summerCost+fallCost+winterCost;
+        System.out.println("Your yearly maintenence cost is $" + yearlyCost);
     }
 }
